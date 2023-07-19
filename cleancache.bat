7d6fdca3cc9fef61c9aa315b768149e053cc101f
Pedro Silva <73768611+Peqzzz@users.noreply.github.com>
2023-07-19 02:46:58 +0100

    Add files via upload

---
+++ cleancache.bat
@@ -0,0 +1,14 @@
+                                                ##LIMPAR CACHE BY PEQZ## 
+
+
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\cache"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\nui-storage"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\server-cache"
+rmdir /s /q "%LocalAppData%\FiveM\FiveM.app\data\server-cache-priv"
+
+
+
+
+
+                                                
+#############################################################https://discord.gg/AfUT9uDMjP############################
